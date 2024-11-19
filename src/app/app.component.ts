2f685d672cf6e051decb6d3b0542a39104772e6e
AmitJ <amitjgtp2@gmail.com>
2024-11-19 11:09:45 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ChildComponent } from './child/child.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ChildComponent,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'AngularAssignment8';
+
+  // First part
+  // public ChildMssg:any;
+
+  // Second part
+  // public msgForChild="Hello Child. This message is sent by Parent"
+  // public msgFromChild:any
+
+  // Third part
+  // public data:string=""
+
+  // public strLen(){
+  //   return this.data.length
+  // }
+
+  //Fourth part
+  public msgLength:number=0
+  public calcLen(msg:any){
+    this.msgLength=msg.length
+    return this.msgLength
+  }
 }
---
+++ src/app/child/child.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, Output,Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-child',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './child.component.html',
+  styleUrl: './child.component.css'
+})
+export class ChildComponent {
+  // First part
+  // @Output() public sendEvent=new EventEmitter()
+
+  // public sendMessage(mssg:any){
+  //   this.sendEvent.emit(mssg)
+  // }
+
+  //Second part
+  @Input() public parentsMsg:any
+
+  @Output() public sendEvent=new EventEmitter()
+  public sendMsg(){
+    this.sendEvent.emit("Hello from Child")
+  }  
+}
